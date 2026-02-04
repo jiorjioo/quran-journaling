1e1eb0885e3afb17507682af36a22b8c1bb57e9e
jiorjioo <balmabahira27@gmail.com>
2026-02-05 00:18:19 +0700

    Quartz sync: Feb 5, 2026, 12:18 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Qur'an Journaling Archive",
+    pageTitle: "Between Verses 🪶",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
